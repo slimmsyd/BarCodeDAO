4de2188cf64efbae68691159dfd6f3a0c9ee1e82
slimmsyd <ssanderss444@gmail.com>
2025-11-18 10:14:03 -0500

    Updatd teh form page from requested updates

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -223,67 +223,69 @@ export default function Home() {
               WebkitBackdropFilter: 'blur(16px) saturate(140%)',
             }}
           >
-            <h2 className="mb-3 text-xl font-bold text-white">
-              EARLY ACCESS
-            </h2>
-            <ul className="space-y-1.5 text-sm text-gray-300 md:text-base">
-              <li className="flex items-start">
-                <svg
-                  className="mr-3 mt-1 h-5 w-5 flex-shrink-0 text-[#53361C]"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <span>Private & Secure Process</span>
-              </li>
-              <li className="flex items-start">
-                <svg
-                  className="mr-3 mt-1 h-5 w-5 flex-shrink-0 text-[#53361C]"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <span>Few carefully curated steps</span>
-              </li>
-              <li className="flex items-start">
-                <svg
-                  className="mr-3 mt-1 h-5 w-5 flex-shrink-0 text-[#53361C]"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <span>Takes 5-10 minutes to complete</span>
-              </li>
-              <li className="flex items-start">
-                <svg
-                  className="mr-3 mt-1 h-5 w-5 flex-shrink-0 text-[#53361C]"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <span>Progress automatically saved</span>
-              </li>
-            </ul>
+            <div className="mx-auto w-full max-w-[420px]">
+              <h2 className="mb-3 text-xl font-bold text-white">
+                EARLY ACCESS
+              </h2>
+              <ul className="space-y-1.5 text-sm text-gray-300 md:text-base">
+                <li className="flex items-start">
+                  <svg
+                    className="mr-3  h-5 w-5 flex-shrink-0 text-[#53361C]"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span>Private & Secure Process</span>
+                </li>
+                <li className="flex items-start">
+                  <svg
+                    className="mr-3  h-5 w-5 flex-shrink-0 text-[#53361C]"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span>Few carefully curated steps</span>
+                </li>
+                <li className="flex items-start">
+                  <svg
+                    className="mr-3  h-5 w-5 flex-shrink-0 text-[#53361C]"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span>Takes 5-10 minutes to complete</span>
+                </li>
+                <li className="flex items-start">
+                  <svg
+                    className="mr-3  h-5 w-5 flex-shrink-0 text-[#53361C]"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span>Progress automatically saved</span>
+                </li>
+              </ul>
+            </div>
           </motion.div>
      
 
